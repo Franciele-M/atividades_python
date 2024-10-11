16424f02c9de36b95653c7c8bf8648962c0ec781
Franciele-M <francielemuniz100@gmail.com>
2024-10-10 21:22:04 -0300

    Adiciona novo arquivo

---
+++ py game/clickX_O.py
@@ -0,0 +1,47 @@
+#Título: Nomear a janela
+
+# Example file showing a basic pygame "game loop"
+#Titulo: Click na tela e veja a mensagem e mude de cor do setup
+import pygame
+
+# pygame setup
+pygame.init()
+screen = pygame.display.set_mode((500, 500))
+clock = pygame.time.Clock()
+running = True
+cor_fundo = 1 #azul
+
+# Altera o título da janela
+pygame.display.set_caption("Jogo da Velha")
+
+while running:
+    # poll for events
+    # pygame.QUIT event means the user clicked X to close your window
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+        # Lógica
+        if event.type == pygame.MOUSEBUTTONDOWN: #mousebuttondown = click do mouse
+            print("Clicou!")
+            cor_fundo = cor_fundo + 1 #contador
+
+            if(cor_fundo > 3):
+                cor_fundo = 1
+        
+
+    if cor_fundo == 1:
+        screen.fill("blue")
+
+    elif cor_fundo == 2:
+        screen.fill("red")
+    else:
+        screen.fill("purple")
+
+
+    # flip() the display to put your work on screen
+    pygame.display.flip()
+
+    clock.tick(60)  # limits FPS to 60 / 60 fps por segundos. quanto mais fps por segundo, mas fluído fica a imagem; quanto menos, menor a qualidade
+
+pygame.quit()
