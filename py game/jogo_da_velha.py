555915a38901f1c90f7568b8e7fca8eacf1df8ce
Franciele <114250487+Franciele-M@users.noreply.github.com>
2024-10-31 22:25:05 -0300

    atualização

--- py game/jogo_da_velha.py
+++ py game/jogo_da_velha.py
@@ -1,4 +1,4 @@
-#Título: Jogo da Velha - Minha Lógica
+#Título: Jogo da Velha - Minha Lógica 
 
 
 import pygame #importa a biblioteca pygame para o script
@@ -9,11 +9,6 @@ pygame.font.init()  #inicalização da fonte no pygame (init = inicio)
 screen = pygame.display.set_mode((600, 600)) #configura o tamanho da tela
 pygame.display.set_caption("Jogo da Velha") # Altera o título da janela
 clock = pygame.time.Clock() #biblioteca tempo
-
-#q1 = (x > 0) and (x < 200) and (y < 200)
-#q2 = (x >= 200) and (x < 400) and (y < 200)
-
-
 running = True #variável de controle de status do jogo
 cor_fundo = 1 #azul / contador
 rodadas = 0
@@ -35,27 +30,18 @@ personagem_x = fonte_quadrinhos.render("X", True, "Blue") #Renderiza o "X" e o d
 personagem_o = fonte_quadrinhos.render("O", True, "Purple") #Renderiza o "O" e o deixa amarela
 
 #variável do personagem atual
-personagem_atual = personagem_o
+personagem_atual = personagem_x
 
-
-
-
-# Dicionário para armazenar o estado dos quadrantes
-quadrantes = {
-    "q1": None,
-    "q2": None,
-    "q3": None,
-    "q4": None,
-    "q5": None,
-    "q6": None,
-    "q7": None,
-    "q8": None,
-    "q9": None,
-}
-
-
-
-
+#quadrantes
+q1 = ''
+q2 = ''
+q3 = ''
+q4 = ''
+q5 = ''
+q6 = ''
+q7 = ''
+q8 = ''
+q9 = ''
 
 
  # função ...        (aqui é a variável)
@@ -74,38 +60,53 @@ def desenha_tabuleiro(espessura, cor):
 
 
 def faz_jogada():
-     #         Jogadores:      Eixo  x , y
+    #             Jogadores:      Eixo  x , y
+    global q1, q2, q3, q4, q5, q6, q7, q8, q9
+    status = True
 
-        if q1 = True: # Primeiro
-            screen.blit(personagem_atual, (70, 40)) # 1/1
+    if q1 == '' and x > 0 and x < 200 and y < 200: # Primeiro
+        screen.blit(personagem_atual, (70, 40)) # 1/1
+        q1 = personagem_atual
 
-        elif q2 = True: # Segundo
-            screen.blit(personagem_atual, (270, 40)) # 2/1
+    elif q2 == '' and x >= 200 and x < 400 and y < 200: # Segundo
+        screen.blit(personagem_atual, (270, 40)) # 2/1
+        q2 = personagem_atual
 
-        elif x >= 400 and y < 200: # Terceiro
-            screen.blit(personagem_atual, (470, 40)) # 3/1
+    elif q3 == '' and x >= 400 and y < 200: # Terceiro
+        screen.blit(personagem_atual, (470, 40)) # 3/1
+        q3 = personagem_atual
 
-        elif x < 200 and y >= 200 and y < 400: # Quarto
-            screen.blit(personagem_atual, (70, 240)) # 1/2
-            
-        elif x >= 200 and x < 400 and y >= 200 and y < 400: # Quinto
-            screen.blit(personagem_atual, (270, 240)) # 2/2
+    elif q4 == '' and x < 200 and y >= 200 and y < 400: # Quarto
+        screen.blit(personagem_atual, (70, 240)) # 1/2
+        q4 = personagem_atual
+        
+    elif q5 == '' and x >= 200 and x < 400 and y >= 200 and y < 400: # Quinto
+        screen.blit(personagem_atual, (270, 240)) # 2/
+        q5 = personagem_atual
 
-        elif x >= 400 and y >= 200 and y < 400: # Sexto
-            screen.blit(personagem_atual, (470, 240)) # 3/2
+    elif q6 == '' and x >= 400 and y >= 200 and y < 400: # Sexto
+        screen.blit(personagem_atual, (470, 240)) # 3/2
+        q6 = personagem_atual
 
-        elif x < 200 and y >= 400: # Sétimo
-            screen.blit(personagem_atual, (70, 440)) # 1/3
+    elif q7 == '' and x < 200 and y >= 400: # Sétimo
+        screen.blit(personagem_atual, (70, 440)) # 1/3
+        q7 = personagem_atual
 
-        elif x >= 200 and x < 400 and y >= 400: # Oitavo
-            screen.blit(personagem_atual, (270, 440)) # 2/3
+    elif q8 == '' and x >= 200 and x < 400 and y >= 400: # Oitavo
+        screen.blit(personagem_atual, (270, 440)) # 2/3
+        q8 = personagem_atual
 
-        elif x >= 400 and y >= 400: # Nono
-            screen.blit(personagem_atual, (470, 440)) # 3/3
-            #screen.blit(mensagem1, (100, 50)) # 3/3
-            #screen.blit(mensagem2, (100, 250)) # 3/3
+    elif q9 == '' and x >= 400 and y >= 400: # Nono
+        screen.blit(personagem_atual, (470, 440)) # 3/3
+        q9 = personagem_atual
 
+        #screen.blit(mensagem1, (100, 50)) # 3/3
+        #screen.blit(mensagem2, (100, 250)) # 3/3
 
+    else:
+        status = False
+
+    return status
 
 
 #Loop do jogo
@@ -127,75 +128,59 @@ while running: # Enquanto a variável "running" for verdadeira, o jogo continua
             print("Eixo Y: ", click_pos[1])
             x = click_pos[0]
             y = click_pos[1]
-            
-            rodadas = rodadas + 1 #contador
-            print(rodadas)
 
-            # if(rodadas > 11): #se o contador for maior que 10
-            #     rodadas = 0 # o contador volta a ser 1
-            
-            #letra = "X" #quando o mouse é clicado o X aparece / define a variável como X
-            # x,y = pygame.mouse.get_pos()
+            if rodadas >= 9:
+                screen.fill("Black")
+                rodadas = 0
+                x = 0
+                y = 0
+                tabuleiro_desenhado = False 
 
-            
-
-            # Altera o persnagem de X para O
-            if personagem_atual == personagem_o:
-                personagem_atual = personagem_x
-
-            else:
-                personagem_atual = personagem_o
+            if (faz_jogada()):
+                rodadas = rodadas + 1 #contador
+                 # Altera o persnagem de X para O
+                if personagem_atual == personagem_x:
+                    personagem_atual = personagem_o
+                else:
+                    personagem_atual = personagem_x
 
 
-            faz_jogada() # ACRESCENTAR 
 
-
-        if tabuleiro_desenhado == False:
-            #Função que desenha tabuleiro (espessura, cor) (aqui é o valor da variável: tamanho, cor, etc.)
-            desenha_tabuleiro(10, "green")
-            tabuleiro_desenhado = True #DESCOMENTA DEPOIS
-
-        
 
 #--------------------------------------------------------------------------------------------------------------------------------------------------------------
 
        
-        if rodadas >= 10:
-            screen.fill("Black")
-            rodadas = 0
-            x = 0
-            y = 0
-            tabuleiro_desenhado = False #DESCOMENTA DEPOIS
-
-
-
-            # Redesenha o tabuleiro
-        #     pygame.draw.line(screen, "white",(200, 0), (200, 600), 7) # desenha uma linha vertical na tela (variável: tela, cor, tamanho, tamanho, expessura)
-        #     pygame.draw.line(screen, "white",(400, 0), (400, 600), 7) # desenha uma linha vertical na tela (variável: tela, cor, tamanho, tamanho, expessura)
         
-        # #                                  3º  1º  ,  4º  , 2º   quadrante
-        #     pygame.draw.line(screen, "white",(0, 200), (600, 200), 7) # desenha uma linha vertical na tela (variável: tela, cor, tamanho, tamanho, expessura)
-        #     pygame.draw.line(screen, "white",(0, 400), (600, 400), 7) # desenha uma linha vertical na tela (variável: tela, cor, tamanho, tamanho, expessura)
+    
+       
 
 
-#------------------------------GAME OVER!!!------------------------------------------
 
-       if 
+    if tabuleiro_desenhado == False:
+        #Função que desenha tabuleiro (espessura, cor) (aqui é o valor da variável: tamanho, cor, etc.)
+        desenha_tabuleiro(10, "green")
+        q1 = ''
+        q2 = ''
+        q3 = ''
+        q4 = ''
+        q5 = ''
+        q6 = ''
+        q7 = ''
+        q8 = ''
+        q9 = ''
+        tabuleiro_desenhado = True #DESCOMENTA DEPOIS
+       
+
+        
+
+
+
+#---------------GAME OVER!!!------------------------------------------
+
+       
 
        
             
-
-        
-
-    # muda a cor e mostra as letras
-    #if cor_fundo == 1: # se o contador for = 1
-      #  screen.blit(personagem_x,(20, 20)) # posiciona o X na tela
-
-    #elif cor_fundo == 2: # se o contador for = 2
-        # screen.fill("red") # a tela fica vermelha
-       # screen.blit(personagem_o,(250, 250)) # posiciona o O na tela
-
-    
     # display para atualizar a página
     pygame.display.flip()
 
