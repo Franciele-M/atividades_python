d1ca0bcb5bb6ee5312f7911844cdade6b99eb35e
Franciele <114250487+Franciele-M@users.noreply.github.com>
2024-11-07 21:01:09 -0300

    Atualização de comentários

--- py game/jogo_da_velha.py
+++ py game/jogo_da_velha.py
@@ -1,4 +1,4 @@
-#Título: Jogo da Velha - Minha Lógica 
+#Título: Jogo da Velha
 
 
 import pygame #importa a biblioteca pygame para o script
@@ -10,21 +10,19 @@ screen = pygame.display.set_mode((600, 600)) #configura o tamanho da tela
 pygame.display.set_caption("Jogo da Velha") # Altera o título da janela
 clock = pygame.time.Clock() #biblioteca tempo
 running = True #variável de controle de status do jogo
-cor_fundo = 1 #azul / contador
 rodadas = 0
 tabuleiro_desenhado = False
 x = 0 
 y = 0
 
-fonte_mensagem = pygame.font.SysFont("Comic Sans MS", 150)
+fonte_mensagem = pygame.font.SysFont("Comic Sans MS", 150) # Tipo de fonte e tamanho 
 
-mensagem1 = fonte_mensagem.render("Game ", True, "Yellow")
+# Mensagens que vai aparecer: "Game Over!" e "Velha!"
+mensagem1 = fonte_mensagem.render("Game ", True, "Yellow") 
 mensagem2 = fonte_mensagem.render("Over! ", True, "Yellow")
 velha = fonte_mensagem.render("Velha! ", True, "Yellow")
 
-
-
-fonte_quadrinhos = pygame.font.SysFont("Comic Sans MS", 80) #importando a fonte o definindo o tamanho dela
+fonte_quadrinhos = pygame.font.SysFont("Comic Sans MS", 80) #importando a fonte e definindo o tamanho dela
 
 # Render = tranforma texto em imagem
 personagem_x = fonte_quadrinhos.render("X", True, "Blue") #Renderiza o "X" e o deixa rosa
@@ -47,7 +45,6 @@ q9 = ''
 
  # função ...        (aqui é a variável)
 def desenha_tabuleiro(espessura, cor):
-    # valor 7 = espesu
      # Desenho do tabuleiro
         #                             Origem    Destino
         #                            ( x, y )  ( x ,  y )
@@ -59,104 +56,110 @@ def desenha_tabuleiro(espessura, cor):
         pygame.draw.line(screen, cor,(0, 400), (600, 400), espessura) # desenha uma linha vertical na tela (variável: tela, cor, tamanho, tamanho, expessura)
 
 
-
+# Lógica para preencher os quadrantes com X ou O
 def faz_jogada():
-    #             Jogadores:      Eixo  x , y
+
     global q1, q2, q3, q4, q5, q6, q7, q8, q9
     status = True
 
     if q1 == '' and x > 0 and x < 200 and y < 200: # Primeiro
         screen.blit(personagem_atual, (70, 40)) # 1/1
-        q1 = personagem_atual
+        q1 = personagem_atual # Personagem atual (X ou O) ocupando esse quadrante
 
     elif q2 == '' and x >= 200 and x < 400 and y < 200: # Segundo
         screen.blit(personagem_atual, (270, 40)) # 2/1
-        q2 = personagem_atual
+        q2 = personagem_atual # Personagem atual (X ou O) ocupando esse quadrante
 
     elif q3 == '' and x >= 400 and y < 200: # Terceiro
         screen.blit(personagem_atual, (470, 40)) # 3/1
-        q3 = personagem_atual
+        q3 = personagem_atual # Personagem atual (X ou O) ocupando esse quadrante
 
     elif q4 == '' and x < 200 and y >= 200 and y < 400: # Quarto
         screen.blit(personagem_atual, (70, 240)) # 1/2
-        q4 = personagem_atual
+        q4 = personagem_atual # Personagem atual (X ou O) ocupando esse quadrante
         
     elif q5 == '' and x >= 200 and x < 400 and y >= 200 and y < 400: # Quinto
-        screen.blit(personagem_atual, (270, 240)) # 2/
-        q5 = personagem_atual
+        screen.blit(personagem_atual, (270, 240)) # 2/2
+        q5 = personagem_atual # Personagem atual (X ou O) ocupando esse quadrante
 
     elif q6 == '' and x >= 400 and y >= 200 and y < 400: # Sexto
         screen.blit(personagem_atual, (470, 240)) # 3/2
-        q6 = personagem_atual
+        q6 = personagem_atual # Personagem atual (X ou O) ocupando esse quadrante
 
     elif q7 == '' and x < 200 and y >= 400: # Sétimo
         screen.blit(personagem_atual, (70, 440)) # 1/3
-        q7 = personagem_atual
+        q7 = personagem_atual # Personagem atual (X ou O) ocupando esse quadrante
 
     elif q8 == '' and x >= 200 and x < 400 and y >= 400: # Oitavo
         screen.blit(personagem_atual, (270, 440)) # 2/3
-        q8 = personagem_atual
+        q8 = personagem_atual # Personagem atual (X ou O) ocupando esse quadrante
 
     elif q9 == '' and x >= 400 and y >= 400: # Nono
         screen.blit(personagem_atual, (470, 440)) # 3/3
-        q9 = personagem_atual
-
-        #screen.blit(mensagem1, (100, 50)) # 3/3
-        #screen.blit(mensagem2, (100, 250)) # 3/3
+        q9 = personagem_atual # Personagem atual (X ou O) ocupando esse quadrante
 
     else:
         status = False
-
     return status
 
 
+# Verifica se os qudrantes estão preenchidos para determinar um vencedor ou se deu velha
 def check_vencedor():
     status = False
-    if q1 == q2 == q3 != '': 
-        print("Vencedor1", personagem_atual) # Linhas
+    if q1 == q2 == q3 != '': # Se os 3 primeiros quadrantes estiverem preenchidos e não estiverem vazios:
         status = True
-        pygame.draw.line(screen, "Red",(0, 100), (600, 100), 7)
+        pygame.draw.line(screen, "Red",(0, 100), (600, 100), 7) # aparece uma linha vermelha na horizontal desses 3 primeiros quadrantes
+        screen.blit(mensagem1, (100, 50)) # a mensagem "Game" aparecerá na tela
+        screen.blit(mensagem2, (100, 250))  # a mensagem "Over!" aparecerá na tela
 
+        #  Acontece a mesma coisa acima com os outros quadrantes seguindo a lógica abaixo:
     elif q4 == q5 == q6 != '':
-        print("Vencedor2", personagem_atual) # Linhas
         status = True
         pygame.draw.line(screen, "Red",(0, 300), (600, 300), 7)
+        screen.blit(mensagem1, (100, 50)) 
+        screen.blit(mensagem2, (100, 250)) 
 
     elif q7 == q8 == q9 != '':
-        print("Vencedor3", personagem_atual) # Linhas
         status = True
         pygame.draw.line(screen, "Red",(0, 500), (600, 500), 7)
+        screen.blit(mensagem1, (100, 50)) 
+        screen.blit(mensagem2, (100, 250)) 
 
     elif q1 == q4 == q7 != '':
-        print("Vencedor4", personagem_atual) # Colunas
         status = True
         pygame.draw.line(screen, "Red",(100, 0), (100, 600), 7) 
+        screen.blit(mensagem1, (100, 50)) 
+        screen.blit(mensagem2, (100, 250)) 
 
     elif q2 == q5 == q8 != '':
-        print("Vencedor5", personagem_atual) # Colunas
         status = True
         pygame.draw.line(screen, "Red",(300, 0), (300, 600), 7) 
+        screen.blit(mensagem1, (100, 50)) 
+        screen.blit(mensagem2, (100, 250)) 
 
     elif q3 == q6 == q9 != '':
-        print("Vencedor6", personagem_atual) # Colunas
         status = True
         pygame.draw.line(screen, "Red",(500, 0), (500, 600), 7) 
+        screen.blit(mensagem1, (100, 50)) 
+        screen.blit(mensagem2, (100, 250)) 
 
     elif q1 == q5 == q9 != '':
-        print("Vencedor7", personagem_atual) # Diagonal
         status = True
         pygame.draw.line(screen, "Red", (1, 1), (900, 880), 7)
+        screen.blit(mensagem1, (100, 50)) 
+        screen.blit(mensagem2, (100, 250)) 
 
     elif q3 == q5 == q7 != '':
-        print("Vencedor8", personagem_atual) # Diagonal
         status = True
         pygame.draw.line(screen, "Red", (10, 600), (600, 10), 7)
+        screen.blit(mensagem1, (100, 50)) 
+        screen.blit(mensagem2, (100, 250)) 
 
     if not status:
         # Se todos os quadrantes estiverem preenchidos e ninguém venceu, é "VELHA"
+        # Se todos os quadrantes forem diferente de vazio e não se encaixar nas condições (elif) anteriores:
         if q1 != '' and q2 != '' and q3 != '' and q4 != '' and q5 != '' and q6 != '' and q7 != '' and q8 != '' and q9 != '':
-            print("VELHA", personagem_atual)
-            screen.blit(velha, (80, 150))  
+            screen.blit(velha, (80, 150))   # Aparece a mensagem "Velha!"
     
     return status
 
@@ -174,7 +177,6 @@ while running: # Enquanto a variável "running" for verdadeira, o jogo continua
         # Lógica para click
         # MOUSEBUTTONDOWN significa evento do click do mouse
         if event.type == pygame.MOUSEBUTTONDOWN: # Se o mouse for clicado
-            #print("Clicou!") # Aparecerá "clicou!" no terminal
             click_pos = pygame.mouse.get_pos() # traz a posição do mouse no evento click
 
            # print("Eixo X: ", click_pos[0]) #printa a posição do eixo X
